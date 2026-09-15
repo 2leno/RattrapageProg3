3ea284482ec07667e61639cfd41a77b46aafcd88
Lucas Andrianina <lucasandriamanga@gmail.com>
2026-09-15 10:25:01 +0300

    Add mappers for entity to DTO conversion
    * Add entity classes and enums for Driver, Vehicle, Trip
    
    * Add DTOs for request and response objects
    
    * Add mappers for entity to DTO conversion

---
+++ src/main/java/mg/hei/kofia/mapper/TripMapper.java
@@ -0,0 +1,34 @@
+package mg.hei.kofia.mapper;
+
+import org.springframework.stereotype.Component;
+import mg.hei.kofia.entity.Trip;
+import mg.hei.kofia.entity.dto.TripResponse;
+
+@Component
+public class TripMapper {
+
+    private final DriverMapper driverMapper;
+    private final VehicleMapper vehicleMapper;
+
+    public TripMapper(DriverMapper driverMapper, VehicleMapper vehicleMapper) {
+        this.driverMapper = driverMapper;
+        this.vehicleMapper = vehicleMapper;
+    }
+
+    public TripResponse toTripResponse(Trip trip) {
+        if (trip == null) {
+            return null;
+        }
+        return TripResponse.builder()
+                .id(trip.getId())
+                .driver(driverMapper.toDriverSummary(trip.getDriver()))
+                .vehicle(vehicleMapper.toVehicleSummary(trip.getVehicle()))
+                .tripDate(trip.getTripDate().toString())
+                .departureCity(trip.getDepartureCity())
+                .arrivalCity(trip.getArrivalCity())
+                .distanceKm(trip.getDistanceKm())
+                .billedAmount(trip.getBilledAmount())
+                .status(trip.getStatus().name())
+                .build();
+    }
+}
