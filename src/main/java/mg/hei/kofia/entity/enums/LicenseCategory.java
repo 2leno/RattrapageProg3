32a67918277b86ab1b0efd530f578ccbb8ef2092
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-09-15 10:04:53 +0300

    Add entity classes and enums for Driver, Vehicle, Trip

---
+++ src/main/java/mg/hei/kofia/entity/enums/LicenseCategory.java
@@ -0,0 +1,7 @@
+package mg.hei.kofia.entity.enums;
+
+public enum LicenseCategory {
+    B,
+    C,
+    D
+}
---
+++ src/main/java/mg/hei/kofia/entity/enums/VehicleType.java
@@ -0,0 +1,7 @@
+package mg.hei.kofia.entity.enums;
+
+public enum VehicleType {
+    FOURGON,
+    CAMION,
+    BENNE
+}
