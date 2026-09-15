0fa66261b8eba55c1ddc99af804374a3098dbfe1
Lucas Andrianina ANDRIAMANGA <lucasandriamanga@gmail.com>
2026-09-15 09:16:17 +0300

    Add custom exceptions for error handling

---
+++ src/main/java/school/hei/rattrapageprog3/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package school.hei.rattrapageprog3.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
